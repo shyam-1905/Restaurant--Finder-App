11fa525dc9c683199ed26c0610e91886b6c5944d
shyam-1905 <yennashyamprasadreddy19@gmail.com>
2024-06-23 15:28:20 +0530

    changes in notfound page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -6,7 +6,7 @@ export default function notFound() {
       <div className="bg-white p-8 rounded-lg shadow-lg text-center">
         <h1 className="text-6xl font-bold text-blue-500">404</h1>
         <p className="mt-4 text-2xl font-semibold">Page Not Found</p>
-        <p className="mt-2 text-gray-600">The page you're looking for doesn't exist.</p>
+        <p className="mt-2 text-gray-600">The page you are looking for doesnot exist.</p>
         <Link href="/" className="mt-6 inline-block px-6 py-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600">
             Go Back Search
         </Link>
